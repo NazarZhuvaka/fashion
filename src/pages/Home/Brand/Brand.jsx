a43417ee35511c2ac1d97a30008f9bc1f4f9d1cd
Nazar Zhuvaka <nazar.zhuvaka.mcboss@gmail.com>
2024-03-13 11:42:34 +0300

    commit

--- src/pages/Home/Brand/Brand.jsx
+++ src/pages/Home/Brand/Brand.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import React from "react";
 
 export const Brand = () => {
   return (
-    <div>Brand</div>
-  )
-}
+    <div className="brand">
+      <div className="brand__img">
+        <img src="" alt="women" />
+        <button className="brand__img-btn">Explore now</button>
+      </div>
+
+      <div className="brand__info">
+        <h3 className="brand__info-title">Best fashion brand</h3>
+        <p className="brand__info-desc">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+          ullamcorper congue eros, eget tincidunt ipsum eleifend ut orem ipsum
+          dolor sit amet consectetur adipiscing elit Sed ullamcorper congue eros
+          eleifend ut tincidunt ipsum .
+        </p>
+      </div>
+    </div>
+  );
+};
