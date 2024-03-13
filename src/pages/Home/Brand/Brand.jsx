e3b316b91adb14b606664b5af4d9f80e0c77cee1
Nazar Zhuvaka <nazar.zhuvaka.mcboss@gmail.com>
2024-03-13 11:49:21 +0300

    commit

--- src/pages/Home/Brand/Brand.jsx
+++ src/pages/Home/Brand/Brand.jsx
@@ -16,6 +16,20 @@ export const Brand = () => {
           dolor sit amet consectetur adipiscing elit Sed ullamcorper congue eros
           eleifend ut tincidunt ipsum .
         </p>
+        <div className="brand__info-blocks">
+          <div className="brand__info-blocks_text">
+            <p className="brand__info-blocks_text-title">250+</p>
+            <p className="brand__info-blocks_text-desc">Store Branches</p>
+          </div>
+          <div className="brand__info-blocks_text">
+            <p className="brand__info-blocks_text-title">7000+</p>
+            <p className="brand__info-blocks_text-desc">Product Sold</p>
+          </div>
+          <div className="brand__info-blocks_text">
+            <p className="brand__info-blocks_text-title">3900+</p>
+            <p className="brand__info-blocks_text-desc">5 Star Reviews</p>
+          </div>
+        </div>
       </div>
     </div>
   );
